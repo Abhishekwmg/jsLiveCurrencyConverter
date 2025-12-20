60cb2892b2bd35ff015c6f3f934260653644e4d0
Abhishekwmg <ak89471@gmail.com>
2025-12-20 22:38:42 +0530

    HTML Structure & CSS

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-const options = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'apy-token': 'APY0GWx2bR1QV3Z9OD6gOqW0zEpS4kXitBSOQY0NwCSEKjFfdAmOSCnxw2KVUpNS'
-    },
-    body: '{"source":"eur","target":"inr"}'
-};
+import { countryList } from "./data/countries.js";
+import { cities } from "./data/cities.js";
 
-fetch('https://api.apyhub.com/data/convert/currency', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+// const options = {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'apy-token': 'APY0GWx2bR1QV3Z9OD6gOqW0zEpS4kXitBSOQY0NwCSEKjFfdAmOSCnxw2KVUpNS'
+//     },
+//     body: '{"source":"eur","target":"inr"}'
+// };
+
+// fetch('https://api.apyhub.com/data/convert/currency', options)
+//     .then(response => response.json())
+//     .then(response => console.log(response))
+//     .catch(err => console.error(err));
+
+
+
+const pageLogo = document.querySelector('#page-logo');
+// pageLogo.src = countryList[19].country_img;
---
+++ data/countries.js
@@ -0,0 +1,162 @@
+export const countryList = [
+    {
+        "country": "United States",
+        "country_code": "US",
+        "currency": "US Dollar",
+        "currency_code": "USD",
+        "symbol": "$",
+        "country_img": "./imgs/country-icons/usa.png"
+    },
+    {
+        "country": "India",
+        "country_code": "IN",
+        "currency": "Indian Rupee",
+        "currency_code": "INR",
+        "symbol": "₹",
+        "country_img": "./imgs/country-icons/india.png"
+    },
+    {
+        "country": "United Kingdom",
+        "country_code": "GB",
+        "currency": "Pound Sterling",
+        "currency_code": "GBP",
+        "symbol": "£",
+        "country_img": "./imgs/country-icons/united-kingdom.png"
+    },
+    {
+        "country": "European Union",
+        "country_code": "EU",
+        "currency": "Euro",
+        "currency_code": "EUR",
+        "symbol": "€",
+        "country_img": "./imgs/country-icons/european.png"
+    },
+    {
+        "country": "Japan",
+        "country_code": "JP",
+        "currency": "Japanese Yen",
+        "currency_code": "JPY",
+        "symbol": "¥",
+        "country_img": "./imgs/country-icons/japan.png"
+    },
+    {
+        "country": "China",
+        "country_code": "CN",
+        "currency": "Chinese Yuan",
+        "currency_code": "CNY",
+        "symbol": "¥",
+        "country_img": "./imgs/country-icons/china.png"
+    },
+    {
+        "country": "Canada",
+        "country_code": "CA",
+        "currency": "Canadian Dollar",
+        "currency_code": "CAD",
+        "symbol": "$",
+        "country_img": "./imgs/country-icons/canada.png"
+    },
+    {
+        "country": "Australia",
+        "country_code": "AU",
+        "currency": "Australian Dollar",
+        "currency_code": "AUD",
+        "symbol": "$",
+        "country_img": "./imgs/country-icons/australia.png"
+    },
+    {
+        "country": "Switzerland",
+        "country_code": "CH",
+        "currency": "Swiss Franc",
+        "currency_code": "CHF",
+        "symbol": "CHF",
+        "country_img": "./imgs/country-icons/switzerland.png"
+    },
+    {
+        "country": "Russia",
+        "country_code": "RU",
+        "currency": "Russian Ruble",
+        "currency_code": "RUB",
+        "symbol": "₽",
+        "country_img": "./imgs/country-icons/russia.png"
+    },
+    {
+        "country": "South Korea",
+        "country_code": "KR",
+        "currency": "South Korean Won",
+        "currency_code": "KRW",
+        "symbol": "₩",
+        "country_img": "./imgs/country-icons/south-korea.png"
+    },
+    {
+        "country": "Singapore",
+        "country_code": "SG",
+        "currency": "Singapore Dollar",
+        "currency_code": "SGD",
+        "symbol": "$",
+        "country_img": "./imgs/country-icons/singapore.png"
+    },
+    {
+        "country": "New Zealand",
+        "country_code": "NZ",
+        "currency": "New Zealand Dollar",
+        "currency_code": "NZD",
+        "symbol": "$",
+        "country_img": "./imgs/country-icons/newzealand.png"
+    },
+    {
+        "country": "South Africa",
+        "country_code": "ZA",
+        "currency": "South African Rand",
+        "currency_code": "ZAR",
+        "symbol": "R",
+        "country_img": "./imgs/country-icons/southafrica.png"
+    },
+    {
+        "country": "Brazil",
+        "country_code": "BR",
+        "currency": "Brazilian Real",
+        "currency_code": "BRL",
+        "symbol": "R$",
+        "country_img": "./imgs/country-icons/brazil.png"
+    },
+    {
+        "country": "Mexico",
+        "country_code": "MX",
+        "currency": "Mexican Peso",
+        "currency_code": "MXN",
+        "symbol": "$",
+        "country_img": "./imgs/country-icons/mexico.png"
+    },
+    {
+        "country": "United Arab Emirates",
+        "country_code": "AE",
+        "currency": "UAE Dirham",
+        "currency_code": "AED",
+        "symbol": "د.إ",
+        "country_img": "./imgs/country-icons/united-arab-emirated.png"
+    },
+    {
+        "country": "Saudi Arabia",
+        "country_code": "SA",
+        "currency": "Saudi Riyal",
+        "currency_code": "SAR",
+        "symbol": "﷼",
+        "country_img": "./imgs/country-icons/saudi-arabia.png"
+    },
+    {
+        "country": "Turkey",
+        "country_code": "TR",
+        "currency": "Turkish Lira",
+        "currency_code": "TRY",
+        "symbol": "₺",
+        "country_img": "./imgs/country-icons/turkey.png"
+    },
+    {
+        "country": "Indonesia",
+        "country_code": "ID",
+        "currency": "Indonesian Rupiah",
+        "currency_code": "IDR",
+        "symbol": "Rp",
+        "country_img": "./imgs/country-icons/indonesia.png"
+    }
+]
