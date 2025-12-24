f2e8992ca4a5ca8d9344b59c8375a7e7f4bb1c23
Abhishekwmg <ak89471@gmail.com>
2025-12-24 22:10:51 +0530

    code-optimization

--- app.js
+++ app.js
@@ -31,9 +31,7 @@ chooseCityBtn.addEventListener('click', (e) => {
     dialogBox.showModal();
 })
 
-closeModal.addEventListener('click', () => {
-    dialogBox.close();
-})
+closeModal.addEventListener('click', () => dialogBox.close())
 // pageLogo.src = countryList[19].country_img;
 
 const citiesSuggestion = [];
@@ -48,7 +46,7 @@ cityInput.addEventListener('input', (e) => {
     suggestions.innerHTML = "";
     if (!enteredCity) return;
 
-    suggestedCity.map((city) => {
+    suggestedCity.forEach((city) => {
         suggestions.innerHTML += `<p class="suggested-cities">${city.city}, ${city.country}</p>`
     })
 })
@@ -66,48 +64,6 @@ suggestions.addEventListener('click', (e) => {
     dialogBox.close();
 })
 
-
-// function timeZone() {
-//     const d = new Date();
-//     const days = [
-//         "Sunday",
-//         "Monday",
-//         "Tuesday",
-//         "Wednesday",
-//         "Thursday",
-//         "Friday",
-//         "Saturday"
-//     ];
-
-//     const currDay = days[d.getDay()];
-//     const currDate = d.getDate();
-//     const currMonth = d.getMonth() + 1;
-//     const currYear = d.getFullYear();
-//     const currMinute = d.getMinutes();
-//     const currHour = d.getHours();
-//     const currSecond = d.getSeconds();
-//     // const currTimezone = d.
-
-//     // console.log(`${d}`)
-
-//     const timeZone = new Intl.DateTimeFormat("en-US", {
-//         timeZoneName: "short"
-//     }).format(d);
-
-//     // console.log(timeZone);
-//     const tz = timeZone.split(" ").pop();
-//     // console.log(tz)
-
-//     document.querySelector('.timezone-date').textContent = `${currDay}, ${currDate < 10 ? 0 + currDate : currDate}/${currMonth < 10 ? 0 + currMonth : currMonth}/${currYear}`;
-//     document.querySelector('.timezone-curr-time').textContent = `${currHour < 10 ? "0" + currHour : currHour}:${currMinute < 10 ? "0" + currMinute : currMinute}:${currSecond < 10 ? "0" + currSecond : currSecond} ${tz}`;
-
-//     // console.log(`${currDay}, ${currDate}/${currMonth}/${currYear}`);
-//     // console.log(`${currHour}:${currMinute}:${currSecond} ${tz}`)
-// }
-
-
-// setInterval(() => { timeZone() }, 1000);
-// timeZone();
 
 const dateEl = document.querySelectorAll('.timezone-date');
 const timeEl = document.querySelectorAll('.timezone-curr-time');
@@ -137,13 +93,9 @@ function updateTime() {
     timeEl.forEach((time) => {
         time.textContent = timeFormatter.format(now)
     })
-
-    // dateEl.textContent = dateFormatter.format(now);
-    // timeEl.textContent = timeFormatter.format(now);
 }
 
 setInterval(updateTime, 1000);
-// updateTime(); // run immediately
 
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuOpen = document.querySelector('.menu-open');
@@ -163,53 +115,174 @@ closeMenu.addEventListener('click', (e) => {
     menuOpen.classList.remove('show')
 })
 
-const sourceBtn = document.querySelector('#source-btn');
-const sourceCountryDropdown = document.querySelector('#source-country-dropdown');
-const currencyCountryList = document.querySelector('#currency-country-list');
+// const sourceBtn = document.querySelectorAll('#source-btn');
+// const sourceCountryDropdown = document.querySelectorAll('#source-country-dropdown');
+// const currencyCountryList = document.querySelectorAll('#currency-country-list');
+// const currencySymbol = document.querySelectorAll('.curr-sign');
 
 
-sourceBtn.addEventListener('click', () => {
-    // sourceCountryDropdown.classList.add('show');
-    console.log("clicked")
-    if (sourceCountryDropdown.classList.contains("show")) {
-        sourceCountryDropdown.classList.remove("show")
-    } else {
-        sourceCountryDropdown.classList.add("show")
-    }
+// sourceBtn.forEach((srcbtn) => {
+//     srcbtn.addEventListener('click', () => {
 
-    // currencyCountryList.innerHTML = "";
+//         sourceCountryDropdown.forEach((srcdrop) => {
+//             if (srcdrop.classList.contains("show")) {
+//                 srcdrop.classList.remove("show")
+//             } else {
+//                 srcdrop.classList.add("show")
+//             }
+//         })
 
-    countryList.forEach((countries) => {
-        const { currency, country, country_img } = countries;
-        currencyCountryList.innerHTML += `<li data-currency="${currency}" data-icon="${country_img}">
+//         currencyCountryList.innerHTML = "";
+
+//         countryList.forEach((countries) => {
+//             const { currency, country, country_img, symbol } = countries;
+//             currencyCountryList.forEach((cl) => {
+//                 cl.insertAdjacentHTML('beforeend', `<li data-currency="${currency}" data-icon="${country_img}" data-symbol="${symbol}">
+//                                 <p>${currency}</p>
+//                                 <span><img src="${country_img}" alt="${country}"></span>
+//                             </li>` );
+//             })
+//         })
+//     })
+// })
+
+// currencyCountryList.innerHTML = "";
+
+// currencyCountryList.forEach((list) => {
+//     list.addEventListener("click", (e) => {
+//         const liEl = e.target.closest("li");
+
+//         if (!liEl) return;
+
+//         const chosenCurr = liEl.dataset.currency;
+//         const currIcon = liEl.dataset.icon;
+//         const currSymbol = liEl.dataset.symbol;
+//         console.log(currSymbol);
+//         sourceBtn.forEach((srcbtns) => {
+//             srcbtns.innerHTML = `
+//                         <p>${chosenCurr}</p>
+//                         <div class="moto">
+//                             <img src="${currIcon}" alt="${chosenCurr}">
+//                             <span>
+//                                 <i class="fa-solid fa-caret-down"></i>
+//                             </span>
+//                         </div>
+//                     `;
+//         })
+//         currencySymbol.textContent = currSymbol;
+//         sourceCountryDropdown.forEach((sc) => {
+//             sc.classList.remove("show")
+//         })
+
+//     })
+// })
+
+// const revertCurr = document.querySelector('.revert');
+// revertCurr.addEventListener('click', (e) => {
+//     console.log("clicked")
+//     const revertEl = document.querySelector('.split-button-drops');
+//     revertEl.style.flexDirection = revertEl.style.flexDirection === "row-reverse" ? "row" : "row-reverse";
+// })
+
+// //New Logic
+
+// document.querySelectorAll('.btn-wid').forEach((btns) => {
+//     const srcBtn = document.querySelector('.source-btn');
+//     const genCountry = document.querySelector('.generate-country');
+//     const currCountryList = document.querySelector('.currency-country-list');
+//     const currSign = document.querySelector('.curr-sign');
+
+//     srcBtn.addEventListener('click', () => {
+
+//         genCountry.classList.toggle("show");
+//     })
+
+// })
+
+
+const btnSourceModel = document.querySelector("#btn-source-model");
+const sourceBtn = document.querySelector('#src-btn');
+const sourceCountryDropdown = document.querySelector("#src-country-dropdown");
+const sourceCurrencyCountryList = document.querySelector("#source-currency-country-list");
+const sourceCurrencySign = document.querySelector("#source-curr-sign");
+
+
+
+async function fetchCurrDetails(source, target) {
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'apy-token': 'APY0GWx2bR1QV3Z9OD6gOqW0zEpS4kXitBSOQY0NwCSEKjFfdAmOSCnxw2KVUpNS'
+        },
+        body: JSON.stringify({ "source": source, "target": target })
+    };
+    const getData = await fetch('https://api.apyhub.com/data/convert/currency', options);
+    const data = await getData.json();
+    console.log(data);
+}
+
+// fetchCurrDetails("USD", "BTC");
+
+
+let sourceCurrency;
+let targetCurrency;
+
+//code for handling the sourceCurrency,
+//end goal is to get the source currency selected:
+
+//selection of elements;
+const srcBtn = document.querySelector('.src-btn');
+const generateCountry = document.querySelector('.generate-country');
+const srcCurrencyCountryList = document.querySelector('.source-currency-country-list');
+const srcCurrIcon = document.querySelector('#src-curr-icon');
+
+srcBtn.addEventListener('click', () => {
+    generateCountry.classList.toggle("show");
+    srcCurrencyCountryList.innerHTML = "";
+    countryList.forEach(({ currency, country_img, country, symbol, currency_code }) => {
+        srcCurrencyCountryList.insertAdjacentHTML('beforeend', `<li data-currency="${currency}" data-icon="${country_img}" data-currcode="${currency_code}" data-symbol="${symbol}">
                                 <p>${currency}</p>
-                                <span><img src="${country_img}" alt="${country}"></span>
-                            </li>`
+                                 <span><img src="${country_img}" alt="${country}"></span>
+                             </li>`);
     })
-})
 
-currencyCountryList.addEventListener("click", (e) => {
-    const liEl = e.target.closest("li");
+    srcCurrencyCountryList.addEventListener('click', (e) => {
+        let liEl = e.target.closest('li');
 
-    if (!liEl) return;
+        let currency = liEl.dataset.currency;
+        let country = liEl.dataset.country;
+        let symbol = liEl.dataset.symbol;
+        let currImg = liEl.dataset.icon
+        let srcCurrCode = liEl.dataset.currcode;
 
-    const chosenCurr = e.target.dataset.currency;
-    const currIcon = e.target.dataset.icon;
-    sourceBtn.innerHTML = `
-                        <p>${chosenCurr}</p>
+        if (!liEl) return;
+
+        srcBtn.innerHTML = `<p>${currency}</p>
                         <div class="moto">
-                            <img src="${currIcon}" alt="${chosenCurr}">
+                            <img src="${currImg}" alt="">
                             <span>
                                 <i class="fa-solid fa-caret-down"></i>
                             </span>
-                        </div>
-                    `;
+                        </div>`
+        generateCountry.classList.remove("show");
+        srcCurrIcon.textContent = symbol;
+        sourceCurrency = srcCurrCode;
+    })
+});
 
-})
 
-const revertCurr = document.querySelector('.revert');
-revertCurr.addEventListener('click', (e) => {
-    console.log("clicked")
-    const revertEl = document.querySelector('.split-button-drops');
-    revertEl.style.flexDirection = revertEl.style.flexDirection === "row" ? "row-reverse" : "row";
-})
+
+
+//code for handling targetCurrency
+//end goal is to get the target currency
+
+
+//code structure
+//sourceCurrencyBtn -> Click Event -> Open a custom dropdown -> Select currency from that dropdown -> update sourceCurrencyBtn text && update global sourceCurrency value
+//same for the targetCurrencyBtn
+
+// once sourceCurrency and targetCurrency is matched
+// run the fetchCurrencyData function to get the exchange rate
+//calculate and update UI
